42769779b3664634700744ad3f50ee5a21486c28
KrystianKozlowski111 <54074452+KrystianKozlowski111@users.noreply.github.com>
2021-06-17 20:12:58 +0200

    .

--- src/CurrencyConverter.jsx
+++ src/CurrencyConverter.jsx
@@ -4,28 +4,49 @@ import axios from 'axios';
  
 function CurrencyConverter() {
   const [rate, setRate] = useState();
-  const [errDest, seterrDest] = useState('t');
-  const [errSource, seterrSource] = useState('t');
-  const [currencyDate, setcurrencyDate] = useState();
-  const [currencySource, setcurrencySource] = useState();
-  const [currencyDest, setcurrencyDest] = useState();
+  const [showErrors, setshowErrors]=useState(false);
+  const [errDest, seterrDest] = useState('');
+  const [errSource, seterrSource] = useState('');
+  const [currencyDateError, setcurrencyDateError] = useState(false);
+  const [currencyDate, setcurrencyDate] = useState('');
+  const [currencySource, setcurrencySource] = useState('');
+  const [currencyDest, setcurrencyDest] = useState('');
+
 
   const setcurrencyDateInput = (event) => {
-    setcurrencyDate(event.target.value);
+    var m = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+   
+    if(m.test(event.target.value)) {
+      setshowErrors(false);
+      setcurrencyDate(event.target.value);
+      setcurrencyDateError(false);
+    }
+    else {
+      setcurrencyDateError(true);
+      setcurrencyDate(event.target.value);
+    }
+  }
+  const resetFields = () => {
+    setcurrencySource('');
+    setcurrencyDest('');
+    setcurrencyDate('');
   }
   const setcurrencySourceInput = (event) => {
     setcurrencySource(event.target.value.toUpperCase());
   }
+
   const setcurrencyDestInput = (event) => {
     setcurrencyDest(event.target.value.toUpperCase());
   }
 
   const getRates = (event) => {
     event.preventDefault()
+    setshowErrors(true);
     if(currencyDate){
     axios
       .get(`http://api.exchangeratesapi.io/v1/${currencyDate}?access_key=670429a2b2b9b15500a52d8d1eab2c6a`)
-      .then(res => {     
+      .then(res => {   
+          
         const errDest = res.data.rates[currencyDest];
         seterrDest(errDest)
         const errSource = res.data.rates[currencySource];
@@ -47,17 +68,23 @@ function CurrencyConverter() {
 
  
   return (
+    console.log(currencyDate),
     <>
     <form onSubmit={getRates}>
-      <input type="text" maxLength="10" className='currency-date' defaultValue={currencyDate} onChange={setcurrencyDateInput}></input>
-      <input type="text" maxLength="3" style={{textTransform: "upperCase"}} className='currency-source' defaultValue={currencySource} onChange={setcurrencySourceInput}></input>
-      <input type="text" maxLength="3" style={{textTransform: "upperCase"}} className='currency-destination' defaultValue={currencyDest}  onChange={setcurrencyDestInput}></input>
+      <input type="text" maxLength="10" className='currency-date' value={currencyDate} onChange={setcurrencyDateInput}></input>
+      <input type="text" maxLength="3" style={{textTransform: "upperCase"}} className='currency-source' value={currencySource} onChange={setcurrencySourceInput}></input>
+      <input type="text" maxLength="3" style={{textTransform: "upperCase"}} className='currency-destination' value={currencyDest}  onChange={setcurrencyDestInput}></input>
       <button className='find-rate'>Find rate</button>
      
     </form>
-    {errDest!==undefined && errSource!==undefined?<input readOnly className='conversion-result' defaultValue={rate}></input>:''}
-      
+    <button className='reset-fields' onClick={resetFields}>Reset fields</button>
+    {errDest!==undefined && currencyDateError!==true && errSource!==undefined?<input readOnly className='conversion-result' defaultValue={rate}></input>:''}
 
+    
+
+
+      
+    {currencyDateError===true && showErrors?<p >time data {currencyDate} does not match format '$y-$m-$d' </p>:''}
       {errDest===undefined?<p>Symbols '{currencyDest}' are invalid for date {currencyDate} </p>:''}
       {errSource===undefined?<p>Symbols '{currencySource}' are invalid for date {currencyDate} </p>:''}
     </>
